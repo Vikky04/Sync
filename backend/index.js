79f5ad521ecbe20258b43532ac4f0c8bbb3b381c
Vikas Bishnoi <114574074+Vikky04@users.noreply.github.com>
2025-03-10 17:05:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());// req -> res-> json data
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOpations={
-    origin:'http://localhost:5173',
+    origin:'https://sync-vp51.onrender.com',
     credentials:true
 }
 app.use(cors(corsOpations));
